d1bb98f5a128e39797195aa7b4556adf8ca6c8ec
Damar <damarsepti627@gmail.com>
2023-11-02 13:31:02 +0700

    Writing A Custom List Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {
   Resource,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
-import { UserList } from "./components/User/users";
+import { UserList } from "./components/User/UserList";
 
 export const App = () => <Admin dataProvider={dataProvider}>
   <Resource name="users" list={UserList} />
